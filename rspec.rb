ecb3cb8f85d9a4a9038a2de7943290061537be73
André Luis <andrehjr@gmail.com>
2009-05-17 16:47:32 -0300

    Matcher DSL and Custom Matchers from Scratch

--- rspec.rb
+++ rspec.rb
@@ -3,35 +3,89 @@ require 'spec'
 
 class User
   attr_accessor :name, :login
+
+#  def logged?
+#    true
+#  end
 end
 
-Spec::Matchers.create :be_in_zone do |zone|
-     match do |player|
-  player.in_zone?(zone)
+#Using Matchers DSL
+#create is deprecated, use define instead
+Spec::Matchers.define :be_logged do 
+  match do |user|
+    user.logged?
   end
+
   failure_message_for_should do |player|
-  # generate and return the appropriate string.
+    "Is not logged"
   end
   failure_message_for_should_not do |player|
-  # generate and return the appropriate string.
+    "Shouldn't be logged"
   end
   description do
-  # generate and return the appropriate string.
+    "Some description"
   end
 end
 
-describe "something" do
-  it "should" do
+describe "user" do
+  it "should be logged" do
     @user = mock("user")
     
-    p @user.methods.sort
-    @user.should_receive(:login).with(:lol)
-    @user.login :lol
-  end
-  
-  it "should do something" do 
-    4.should == 4
+    #p @user.methods.sort
+    #@user.should_receive(:login).with(:lol)
+    #@user.login :lol
+    @user.should_receive(:logged?).and_return(true)
+    @user.should be_logged 
   end
 end
 
+#Using Custom Matchers
+class Zombie
+  attr_accessor :name, :type
+  
+  def undead?
+    true
+  end
+end
+
+module ZombieMatchers
+  class MeatEater
+     def initialize(expected)
+       @expected = expected
+     end
+     def matches?(target)
+       @target = target
+       @target.type.eql?(@expected)
+     end
+     
+     def failure_message_for_should
+       "expected #{@target.inspect} to be of type #{@expected}"
+     end
+     def failure_message_for_should_not
+       "expected #{@target.inspect} not to be of type #{@expected}"
+     end
+  end
+  
+  def be_of_type?(expected)
+    MeatEater.new expected
+  end
+end
+
+describe "Zombies" do 
+  #you can include matchers here... or in the Spec::Runner.configure.
+  #include ZombieMatchers 
+  
+  it "should be of type meat eater" do 
+    @zombie = mock("zombie")
+    @zombie.should_receive(:type).and_return(:meat_eater)
+    
+    @zombie.should be_of_type?(:meat_eater) 
+  end
+end
+
+Spec::Runner.configure do |config|
+  config.include(ZombieMatchers)
+end
+
+#Run the examples
 Spec::Runner.run
