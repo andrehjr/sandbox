3503ecde1a462406b1a4a934c9632089415dde12
André Luis <andrehjr@gmail.com>
2009-06-01 23:31:54 -0300

    cleaning

--- erlang/p1.erl
+++ erlang/p1.erl
@@ -1,14 +1,8 @@
 -module (p1).
 -export ([start/2, status/2, valid/0]).
 
-%%valid() -> 
-%%	whereis(noob) =:= undefined.
-
 start(Time,Fun) ->
-%%	if
-%%		valid() =:= true ->
 			register(noob,spawn(fun() -> status(Time,Fun) end)). 
-%%	end.
 
 status(Time, Fun) -> 
 	receive
